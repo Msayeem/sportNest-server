d37035f054de4f6e53f50c8d6337d323f9fa8f3a
Msayeem <msayeem223@gmail.com>
2026-05-17 23:30:51 +0600

    id specific route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 dotenv.config();
@@ -35,7 +35,13 @@ async function run() {
             app.get('/facilities', async(req, res)=>{
                 const result=await destinationCollection.find().toArray();
                 res.json(result);
-            })
+            });
+
+               app.get('/facilities/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
 
 
 
